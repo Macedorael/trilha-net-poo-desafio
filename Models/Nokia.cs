47c36be8cded768998dbedf5478c9244e552962f
Macedorael <raelmacedodasilva@gmail.com>
2025-01-15 15:20:29 -0300

    feat: criação da herança e do metodo abstrato na classe Nokia

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,22 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+            // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero,modelo,imei,memoria) 
+        {
+            
+        }
+
+        public string nomeApp { get; set; }
+        
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} na Nokia...");
+        }
     }
+    
 }
