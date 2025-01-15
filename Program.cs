b156d6465d959b4c40de2b484c79b34e4aa41f03
Macedorael <raelmacedodasilva@gmail.com>
2025-01-15 15:22:15 -0300

    docs: entrega do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero:"123456789", modelo:"Nokia 3310", imei:"123456789",memoria:16);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero:"123456789", modelo:"Iphone 14", imei:"123456789",memoria:16);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
